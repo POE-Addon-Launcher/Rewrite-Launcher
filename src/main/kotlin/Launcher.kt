c66e4348800360dd5b0fec7bf8c7e7326fad8a15
Rizlim <Rizlim@user.noreply.github.com>
2018-12-23 21:39:34 +0100

    First Commit with source

---
+++ src/main/kotlin/Launcher.kt
@@ -0,0 +1,37 @@
+import javafx.application.Application
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.File
+
+/**
+ *
+ */
+class Launcher
+{
+
+    companion object
+    {
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            // These must be up to date.
+            Constants.LAUNCHER_LOCATION = Launcher::class.java.protectionDomain.codeSource.location.toURI().path
+            Constants.LAUNCHER_VERSION = "2.0"
+
+            Constants.init()
+
+            GlobalScope.launch { scanCommonLocations() }
+
+            if (args.isNotEmpty())
+            {
+                for (str in args)
+                {
+                    Constants.LOG_LEVEL = str.toInt()
+                }
+            }
+
+            //FirstPdf.main(args)
+            Application.launch(LaunchUI::class.java, *args)
+        }
+    }
+}
